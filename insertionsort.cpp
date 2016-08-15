da7019b31e8025296528979e25ec7f7e3394e824
caffeine96 <maitreydams@gmail.com>
2016-08-15 13:16:28 +0530

    Add files via upload

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -6,21 +6,22 @@ using namespace std;
 
 int main()
 {
-	
+	ofstream MyExcelFile;
+	MyExcelFile.open("test.csv",ios::out|ios::app);
 	clock_t stop,start;
 	int n;
 	cin>>n;
 	int a[n]; 
      
-    for(int i=0; i<n; i++)
-        a[i] = (rand()%10)+1; 
+	for(int i=0; i<n; i++)
+        	a[i] = (rand()%10)+1; 
          
 	start=clock();
 	
 	for(int i=1;i<n-1;++i)
 	{
 		int temp=a[i];
-		int j=i-1;
+		int j=i-1; 
 		while(j>=0&&a[j]>a[i])
 		{
 			a[j+1]=a[j];
@@ -34,6 +35,6 @@ int main()
 	double msecs=(stop-start)/(double)CLOCKS_PER_SEC;
 	cout<<msecs;
 	
-	
+	MyExcelFile<<n<<"\""<<msecs<<endl;	
 	return 0;
 }
